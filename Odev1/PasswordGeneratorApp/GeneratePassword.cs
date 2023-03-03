d91b477fab6629915605f27e1deac8bdeb3ee3e2
Zeynep Yalçın <112186688+timekeeperzz@users.noreply.github.com>
2023-03-04 02:54:25 +0300

    Add files via upload

---
+++ Odev1/PasswordGeneratorApp/GeneratePassword.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GeneratePasswordApp
+{
+    public class GeneratePassword
+    {
+        private const string Numbers = "0123456789";
+        private const string LowercaseChars = "abcdefghijklmnopqrstuvwxyz";
+        private const string UppercaseChars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        private const string SpecialChars = "!#$@%^&*()";
+
+        private int length = 0; //password length
+        private bool invalid =false;
+        private string Password;
+
+        private readonly Random random;
+        private readonly StringBuilder PasswordBuilder;
+        private readonly StringBuilder CharacterSetBuilder;
+
+        public bool AddNumbers { get; set; }
+        public bool AddLowercaseChars { get; set; }
+        public bool AddUppercaseChars { get; set; }
+        public bool AddSpecialChars { get; set; }
+        public GeneratePassword()
+        {
+            random = new Random();
+            PasswordBuilder= new StringBuilder();
+            CharacterSetBuilder= new StringBuilder();
+
+        }
+        
+        public void Greeeting()
+        {
+            Console.WriteLine(Messages.GreetingAndErrorMessages.Stars);
+            Console.WriteLine(Messages.GreetingAndErrorMessages.WelcomeMessage);
+            Console.WriteLine(Messages.GreetingAndErrorMessages.Stars);
+        }
+        public void QuestionsBeforeCreate()
+        {
+            Console.WriteLine(Messages.GreetingAndErrorMessages.Chitchat);
+
+            AskForAddingChars();
+
+            Console.WriteLine(Messages.Questions.PasswordLength);
+
+            var passwordLength = Console.ReadLine();
+
+            if (!int.TryParse(passwordLength, out length))
+            {
+                invalid = true;
+                return;
+            }
+            invalid = false;
+        }
+        public void AskForAddingChars()
+        {
+            
+            // Ask to add numbers and other chars, then read the answer to create password
+            Console.WriteLine(Messages.Questions.AddNumbers);
+
+            AddNumbers = Console.ReadLine()?.ToLower() == "y";
+
+            Console.WriteLine(Messages.Questions.AddLowercaseCharacters);
+
+            AddLowercaseChars = Console.ReadLine()?.ToLower() == "y";
+
+            Console.WriteLine(Messages.Questions.AddUppercaseCharacters);
+
+            AddUppercaseChars = Console.ReadLine()?.ToLower() == "y";
+
+            Console.WriteLine(Messages.Questions.AddSpecialCharacters);
+
+            AddSpecialChars = Console.ReadLine()?.ToLower() == "y";
+
+            if (!AddNumbers && !AddLowercaseChars && !AddUppercaseChars &&
+               !AddSpecialChars)
+            {
+                Console.WriteLine(Messages.GreetingAndErrorMessages.Invalid);
+                Console.WriteLine(Messages.GreetingAndErrorMessages.TryAgain);
+                AskForAddingChars();
+            }
+        }
+
+
+        public void CreatePassword()
+        {
+            if(invalid== true)
+            {
+                ShowErrorMessage();
+                return;
+            }
+
+            if (AddNumbers)
+                CharacterSetBuilder.Append(Numbers);
+            if (AddLowercaseChars)
+                CharacterSetBuilder.Append(LowercaseChars);
+            if (AddUppercaseChars)
+                CharacterSetBuilder.Append(UppercaseChars);
+            if (AddSpecialChars)
+                CharacterSetBuilder.Append(SpecialChars);
+
+            var SetCharacters=CharacterSetBuilder.ToString();
+            for (int i=0;i<length;i++)
+            {
+                var randomIndex = random.Next(SetCharacters.Length);
+                PasswordBuilder.Append(SetCharacters[randomIndex]);
+            }
+
+            Password=PasswordBuilder.ToString();
+
+
+        }
+
+        private void ShowErrorMessage(string? message = null)
+        {
+            if (string.IsNullOrEmpty(message))
+            {
+                Console.WriteLine(Messages.GreetingAndErrorMessages.Lengthless);
+                return;
+            }
+
+            Console.WriteLine(message);
+
+        }
+        public void GetThePassword()
+        {
+            Console.WriteLine(Messages.GreetingAndErrorMessages.Stars);
+            Console.WriteLine(Messages.GreetingAndErrorMessages.CreatedPassword);
+            Console.WriteLine(Password);
+            Console.WriteLine(Messages.GreetingAndErrorMessages.Stars);
+        }
+       
+
+
+    }
+}
---
+++ Odev1/PasswordGeneratorApp/Messages.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GeneratePasswordApp
+{
+    public static class Messages
+    {
+        public static class GreetingAndErrorMessages
+        {
+            public static string WelcomeMessage => "Welcome to the B E S T  P A S S W O R D  M A N A G E R !";
+            public static string Stars => "********************************************************";
+            public static string Chitchat => "I have some options to create a password for you. Can you answer a few Y/N question?";
+            public static string Invalid => "You say no all questions. How can I generate a password for you now ?";
+            public static string TryAgain => "Can you answer the question again? Please write Y to at least one. :)";
+            public static string Lengthless => "is it possible to have a password with no length? I'm sorry, but I can't create a password for you in this case.";
+            public static string CreatedPassword => "Here is the password I created for you! :)";
+            
+        }
+        public static class Questions
+        {
+            public static string AddNumbers => "Do u want to add Numbers?";
+            public static string AddLowercaseCharacters => "Do u want to add lowercase characters?";
+            public static string AddUppercaseCharacters => "Do u want to add uppercase characters?";
+            public static string AddSpecialCharacters => "Do u want to add special characters?";
+            public static string PasswordLength => "Great! The last question! How long do you want to keep your password length?";
+    
+        }
+
+    }
+}
